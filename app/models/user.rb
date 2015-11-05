f0647093f6427430a2e64e75a04718995a917d83
Todd Channick <toc5012@gmail.com>
2015-11-05 00:20:02 -0500

    Minor change to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
