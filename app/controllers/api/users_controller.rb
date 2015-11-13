e2f12db6af4b87b49f5f5d6f8ce9f0fd84e45ad3
Todd Channick <toc5012@gmail.com>
2015-11-13 14:46:57 -0500

    Initial

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,6 +6,11 @@ class Api::UsersController < ApiController
      render json: users, each_serializer: UserSerializer
    end
 
+   def show
+     user = User.find(params[:id])
+     render json: user.to_json, status: 200
+   end
+
    def create
      user = User.new(user_params)
      if user.save
